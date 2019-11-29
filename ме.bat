997c592421201c81134aa80d38ab74657d5db080
mostovsky <mostovsky@ya.ru>
2019-11-29 16:38:08 +0300

    datasheets from LED dir

--- ме.bat
+++ ме.bat
@@ -1,4 +1,5 @@
 @echo off
 cls
 
-"D:\[data]\[info]\[datasheets]\VT.bat" %1 %2 %3 %4 %5 %6
+"D:\[data]\[info]\[datasheets]\VT_on_the_nout.bat" %1 %2 %3 %4 %5 %6
+"D:\[data]\[info]\[datasheets]\VT_on_the_work.bat" %1 %2 %3 %4 %5 %6
