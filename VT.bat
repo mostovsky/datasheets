e497314eda6a852c135abededb2a90e66b311ba5
mostovsky <mostovsky@ya.ru>
2019-12-01 17:13:48 +0300

    fixed some issue

--- VT.bat
+++ VT.bat
@@ -8,6 +8,12 @@ REM >>>>>>> 865722ecfc2cf0faa1917f1255c1c472c3d8740d
 setlocal enabledelayedexpansion
 REM pause
 
+Set "preset=†_f °_, ¢_d £_u §_l •_t Ò_` ¶_; ß_p ®_b ©_q ™_r ´_k ¨_v ≠_y Æ_j Ø_g ‡_h ·_c ‚_n „_e ‰_a Â_[ Ê_w Á_x Ë_i È_o Í_] Î_s Ï_m Ì_' Ó_. Ô_z Ä_F Å_, Ç_D É_U Ñ_L Ö_T _~ Ü_: á_P à_B â_Q ä_R ã_K å_V ç_Y é_J è_G ê_H ë_C í_N ì_E î_A ï_{ ñ_W ó_X ò_I ô_O ö_} õ_S ú_M ù_' û_. ü_Z"
+REM REM "=ù û_> Å_< <- it's problem
+REM echo preset="%preset%"
+
+REM pause
+
 set "First_input=%~1"
 set "Second_input=%~2"
 set "Third_input=%~3"
@@ -76,7 +82,30 @@ REM echo. && echo.
 REM echo to_find="%to_find%"
 REM echo to_replace="%to_replace%"
 REM echo.
-for /f "tokens=*" %%i in ( 'dir /b ^| find /i "%to_find%" /c') do (if "%%i"=="0" (echo. && echo Nothing_founded for "%to_find%" && set "nothing=true" ) else (echo. && set "nothing=false" && echo Something founded for "%to_find%" [%%i] ))
+for /f "tokens=*" %%i in ( 'dir /b ^| find /i "%to_find%" /c') do (
+	if "%%i"=="0" (
+		echo. && echo Nothing_founded for "%to_find%" && set "nothing=true"
+	) else (
+		echo. && set "nothing=false" && echo Something founded for "%to_find%" [%%i]
+	)
+)
+
+REM if ("%nothing%"=="true")&&("%to_find%"!="") (
+REM REM make only if russian symbols
+	REM Call :_notranslit "%to_find%"
+	REM REM echo Result="!Result!"
+	REM REM pause
+	REM REM set "to_find_new=!Result!"
+	REM REM for /f "tokens=*" %%i in ( 'dir /b ^| find /i "%to_find_new%" /c') do (
+		REM REM if "%%i"=="0" (
+			REM REM echo. && echo Nothing_founded for "%to_find_new%" && set "nothing=true"
+		REM REM ) else (
+			REM REM echo. && set "nothing=false" && echo Something founded for "%to_find_new%" [%%i]
+			REM REM set "to_find=%to_find_new%"
+		REM REM )
+	REM REM )
+REM )
+
 echo.
 
 REM for /f "tokens=*" %%i in ('dir /b ^| find /i "%to_find%"') do (
@@ -129,7 +158,8 @@ if "%nothing%"=="true" (
 	REM ECHO Start last pdf if "enter" or another input to abort
 	
 		echo.
-		echo Input "new" to new search
+		REM echo Input "new" to new search
+		echo Press ENTER to new search, ANY symbol to open last
 		goto input
 		
 	ECHO Start last file if "enter" or another input to abort
@@ -150,8 +180,22 @@ set input=0
 set /p "input= Select: "
 set input=%input: =%
 REM if "%input%"=="" ( echo Starting last file && start "" "%asd%" )
+
+if "%input%"=="" (
+	REM echo open first
+	REM call start "" "%%temp_1%%"
+	echo Open Last
+	echo CN="%CN%"
+	call start "" "%%temp_%CN%%%"
+	goto input
+)
+
 echo "%input%" | find /i "new" > nul
 if "%errorlevel%"=="0" ( echo New search now && goto END )
+
+echo "%input%" | find /i "exit" > nul
+if "%errorlevel%"=="0" ( echo Exit now && pause && goto really_end )
+
 if %input% GEQ 1 (
 REM if %input% LEQ %CN% exit /b %input%
 	if %input% LEQ %CN% (
@@ -167,10 +211,14 @@ REM if %input% LEQ %CN% exit /b %input%
 	)
 ) else (
 	rem zero
-	echo open first
-	call start "" "%%temp_1%%"
+	REM echo open first
+	REM call start "" "%%temp_1%%"
+		REM echo Open Last
+		REM call start "" "%%temp_%CN%%%"
+		REM goto input
+	goto END
 )
-echo Error! Retry input. Input "new" to new search
+REM echo Error! Retry input. Press ENTER to new search, ANY symbol to open last
 goto input
 
 
@@ -196,10 +244,30 @@ REM * in the end print list of AppNotes by tag "use-%APPnote_name%" (in finding 
 REM * in the end print list of USES_WITH by tag "with-%Component_name%". Example: "__TP4056__8Vinmax_TempSenceInput_2led_SOP8_with-8205_with-DW01" use "DW01" driver and "8205" mosfets
 REM * in finding use "SC34063" and "NCV34063" in "PWR_DCDC_1.5A_DWN_UP_INV__MC34063_SC_NCV__"
 REM * in finding "MCP73831(2)" = "MCP73831" and "MCP73832"
-REM * RUS coding in search (KP707 = –ö–ü707)
-REM * In "dcdc" use "ALL" for (LT1370) "It can be operated in all standard switching configurations including boost, buck, flyback, forward, inverting and ‚ÄúCuk.‚Äù"
+REM * RUS coding in search (KP707 = äè707)
+REM += * Translit FIX needed
+REM * In "dcdc" use "ALL" for (LT1370) "It can be operated in all standard switching configurations including boost, buck, flyback, forward, inverting and "Cuk.""
 REM * If nothing found use finding for pre "-" symbol and post "-" symbol
 
 REM in command line use:
 REM cls && set "to_replace=1234__"  && set "to_find=VT_MN_" && setlocal enabledelayedexpansion && for /f "tokens=*" %i in ('dir /b ^| find /i "%to_find%"') do ( echo. & echo. && echo Founded "%i" && echo. && set "asd=%i" && call echo ren "%asd%" "%asd:%to_find%=%to_replace%%" )
-
+REM ___________________________________________________________________________________________________
+:really_end
+cmd "exit /b"
+exit /b
+rem ________________________________________________________________
+REM http://www.cyberforum.ru/cmd-bat/thread805294.html#post4238052
+REM keyboard layout
+:_notranslit
+pause
+Set Data=%~1
+Set Data=%Data: =_%
+Set Result=
+:_loop
+Set Char=%Data:~,1%
+For %%I In (%preset%) Do For /F "tokens=1,2 delims=_" %%A In ("%%I") Do If %Char%==%%A Set Char=%%B
+Set Result=%Result%%Char%
+Set Data=%Data:~1%
+If Defined Data GoTo _loop
+REM GoTo :EOF
+exit /b
